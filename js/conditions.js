0b50b3c68caf8b8088a53e2aefb8302be99f02e3
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 12:23:17 +0200

    warunki if else, if else if

---
+++ js/conditions.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+var wzrostMateusz = 170;
+var wzrostOlgi = 170;
+
+/*Warunek if*/
+
+if (wzrostOlgi < wzrostMateusz){
+    console.log("Olga jest niższa :)");
+}
+
+
+/*Warunek if else*/
+
+if (wzrostOlgi >wzrostMateusz) {
+    console.log ("Olga jest wyższa :)")
+} else {
+    console.log("Olga jest niżasz :)")
+}
+
+
+/*Warunek if else if*/
+
+if (wzrostOlgi > wzrostMateusz) {
+    console.log("Olga jest wyższa :)");
+}  else if (wzrostOlgi == wzrostMateusz) {
+    console.log("Olga tak wysoka jak Mateusz");
+}  else {
+    console.log("Mateusz jest wyższy");
+}
+
